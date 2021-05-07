5609745ca63f1778a645a19f89372eebbe89e162
Swipez1337 <71495304+Swipez1337@users.noreply.github.com>
2021-05-07 18:36:15 +0200

    Update Yukon.c

--- Yukon.c
+++ Yukon.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+struct Card { 
+  char card[3]; //Array med 3 pladser til kulør, kortværdi og nullpointer.
+  int revealed; 
+  struct Node* next; // Pointer to next node in DLL 
+  struct Node* prev; // Pointer to previous node in DLL 
+}; 
+
+/*Datatype for bunke*/
+struct Pile { 
+  char *description; 
+  int index; 
+  struct Pile* next; // Pointer to next Pile 
+  struct Pile* prev; // Pointer to previous Pile 
+  struct Card* head; // Pointer to next node in DLL 
+  struct Card* tail; // Pointer to previous node in DLL 
+}; 
 
 
-int main(){
-char value[14][6]={"Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
-  char suit[4][9]= {"Hearts", "Diamonds", "Clubs", "Spades"};
-   
-  int i=0;
-  int j=0;
-   
-  for (i=0; i<13; i++){
-      for (j=0; j<4; j++){
-           printf("The %s of %s\n\n", value[i], suit[j]);
-      }
-  }
-  system("pause");
-}''
+/**  * All node operations. Skal fungere for hele spillet */ 
+void addPile(); /*Push / Add pile */ 
+void removePile(); /* Pop / Remove pile */
+void addCard(); /*Tilføj et kort. Det skal altid tilføjes til den første bunke af alle bunkerne.  */
+int removeCard(struct Pile *from); /* Fjerner et kort. Returnerer int ifm om der er flere kort i bunken*/
+void moveCard(struct Pile *from, struct Pile *to); /* Ryk et kort fra en bunke til en anden. */
+int moveCards(struct Pile *from, struct Pile *to, int index); /* Rykker flere kort af gangen. Man kan rykke konkrete kort fra en bunke (v int index)*/
+
+/* Logik */
+void shuffle(); /* Shuffle function. */
+void clearDeck(); /* Reset game */
+void generateGame(); /* Start game */
+
+void printDeck(); 
+
+struct Pile *start;
+struct Pile *end; 
+
+int main(void){
+    //Nothing
+}
