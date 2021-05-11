53450dd6384c4b6d243adea1f7c903eec678b334
Swipez1337 <71495304+Swipez1337@users.noreply.github.com>
2021-05-11 13:01:17 +0200

    Update Yukon.c

--- Yukon.c
+++ Yukon.c
@@ -1,78 +1,228 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
-void cardDeck();
-typedef enum { F, T } boolean; // or we could use stdbool.h file
-
-
+/* Data type for the card */
 struct card{
     char value;
     char suit;
     int data;
+    int cardRevealed; 
 struct card* next;
 struct card* prev;
-
 };
 
-struct node{
-    //struct node* next;
-   // struct node* prev;
-    struct card* card;
+/* Data type for the piles */ 
+struct Pile{
+    struct Pile* next;
+    struct Pile* prev;
+    struct card* tail;
+    struct card* head;
 };
 
-struct node nodearray[7];
-struct card cardarray[52];
 
-int topcard =0;
+/***************************************** Function prototyping *****************************************/
+/* Node operation functions */
+void addPile(); /*Push / Add pile */ 
+int removePile(); /* Pop / Remove pile */
+void addCard(struct Pile *location); /*Tilføj et kort. Det skal altid tilføjes til den første bunke af alle bunkerne.  */
+int removeCard(struct Pile *from); /* Fjerner et kort. Returnerer int ifm om der er flere kort i bunken*/
+void moveCard(struct Pile *from, struct Pile *to); /* Ryk et kort fra en bunke til en anden. */
+int moveCards(struct Pile *from, struct Pile *to, int index); /* Rykker flere kort af gangen. Man kan rykke konkrete kort fra en bunke (v int index)*/
+
+/* Game logical functions */
+void shuffleRandom(); /* Random shuffle */
+void splitDeck();   /* Split shuffle */ 
+void clearGame(); /* Reset game */
+void startGame(); /* Start game */
+void basicLD(); /* Loader basic */
+void initializeDeck(); /* Initialiserer piles før kort lægges i */
+void dealCards();  /* Dealer kort ud i 7 bunker */
+void saveDeck();   /* Gemmer decket */
+void quitCurrentGame(); /* Fjerner alle aktuelle piles og kort */
+void quitGame(); /* Lukker progrmamet */
+void helpMessage(); /* Hjælpe besked ifm. validering af brugerinput */
+void invalidInput(); /* Fejlhåndtering af brugerinput */
+void playerInput(); /* Bruger input  */
+void loadDefaultDeck(); /* Load default deck */
+void showDeck(); /* Læg kort på piles */
+
+
+/* Printing operations */
+void printDeck(); /* Load deck */ 
+void printConsole(); /* Basic print console function */
+
+
+
+/***************************************** Global variables *****************************************/
+
+struct Pile *start; // head
+struct Pile *end;  // tail
+
 char inputCommandValue[100];
+char message[100];
 
-//Below function was previously called insertAfter
-    void insertCardsToColumns(int cardonc, int arrayndex) {
-        nodearray[arrayndex].card = &cardarray[topcard];
-        ++topcard;
-        if (cardonc == 0) {
-            return;
-        }
-        struct card *current;
-        struct card *next;
-        struct card *prev;
-        current = nodearray[arrayndex].card;
-        for (int i = 1; i < cardonc; ++i) {
-            if (i == cardonc - 1) {
-                ++topcard;
-                current->prev = prev;
-                return;
-            }
+int main() {
 
-            if (i != 0) {
-                current->prev = &cardarray[topcard - 1];
-            }
-            current->next = &cardarray[topcard];
-            next = current->next;
-            prev = current;
-            current = next;
-            ++topcard;
+    playerInput();
+
+    return 0;
+
+   }
+
+
+/***************************************** Node operations *****************************************/
+
+void addPile() { /*Tilføj pile */ 
+    if(start != NULL){ //Der findes noder i forvejen
+        struct Pile* temp1 = malloc(sizeof(struct Pile));
+        temp1->next = NULL; 
+        temp1->prev = end; 
+        end->next = temp1;
+        end = temp1;
+        end->head = NULL;
+        end->tail = NULL; 
+    } else { //list is empty
+        end = malloc(sizeof(struct Pile));	// setting up space in the memory for the 1st Pile/node.
+        end->prev = NULL;
+        end->next = NULL;
+        start = end;
+        end->head = NULL;
+        end->tail = NULL; 
+    } 
+}
+
+/* Fjerner last pile i doubly linked list */
+int removePile(){
+    if(start != NULL){
+        struct Pile* temp1;
+        if(end->prev != NULL){ //multiple piles remains
+            temp1 = end;
+            end = end->prev;
+            free(temp1);
+            end->next = NULL;
+            return 1;
+        } else { // Only 1 Pile/node remains
+            free(end);
+            end = NULL;
+            start = NULL;
+            return 0;
         }
-        current->prev = prev;
+    } 
+    return 0;
+}
+
+void addCard(struct Pile *location) { /*Tilføj et kort. Det skal altid tilføjes til den første bunke af alle bunkerne.  */ 
+    struct card* temp1 = malloc(sizeof(struct card));
+    if (location->tail != NULL) { //Der findes kort i bunken i forvejen 
+        temp1->prev = location->tail;
+        location->tail->next = temp1;
+        location->tail = temp1;
+    } else { // Der findes ingen kort i bunken
+        location->head = temp1;
+        location->tail = temp1;
     }
+    temp1->cardRevealed = 0;
+}
+ 
+int removeCard(struct Pile *from) { /* Fjerner et kort. Returnerer int ifm om der er flere kort i bunken*/
+    if(from->head == NULL){ // Ingen kort findes
+        return 0; }
+    struct card* temp1;
+    if (from->tail->prev == NULL) { //Only one card
+        temp1 = from->tail;
+        free(temp1);
+        from->head = NULL;
+        from->tail = NULL; 
+        return 0;
+    }
+    //Flere kort mangler / flere kort i bunken
+    temp1 = from->tail;
+    from->tail = from->tail->prev;
+    free(temp1);
+    from->tail->next = NULL; 
+    return 1;
+}
 
-
-void cardDeck() {
-    //opret kort med values og suits
-    int c = 0;
-    char suits[4] = {'C', 'H', 'D', 'S'};
-    for (int j = 0; j < 4; ++j) {
-    for (int i = 1; i < 14; ++i) {
-        struct card tempc;
-        tempc.value = i;
-        cardarray[c].value = i;
-        cardarray[c].suit = suits[j];
-        ++c;
-        }
+void moveCard(struct Pile *from, struct Pile *to) { /* Ryk et kort fra en bunke til en anden. */
+    if (from->head = NULL) { //Ingen kort
+        return; 
+    } 
+    struct card* tempCard; 
+    tempCard = from->tail;
+    if (from->tail->prev == NULL) { //Kun et enkelt kort
+        from->tail = NULL; 
+        from->head = NULL; 
+    } else { // Der er flere end eet kort tilbage
+        from->tail->prev->next = NULL; 
+        from->tail = from->tail->prev;
+        tempCard->prev = NULL; 
+    } 
+    if (to->tail = NULL) { // Hvis "til bunken" er tom, peger pointers i til bunken til den nye node (head/tail)
+        to->head = tempCard; 
+        to->tail = tempCard;
+    } else { // Hvis bunken IKKE er tom
+        to->tail->next = tempCard;
+        tempCard->prev = to->tail;
+        to->tail = tempCard; //to->tail->next
     }
 }
 
+int moveCards(struct Pile *from, struct Pile *to, int index) { /* Rykker flere kort af gangen. Man kan rykke konkrete kort fra en bunke (v int index)*/
+    struct card* tempCard; 
+    int count = 0; 
+
+    if (from->head = NULL) { //Ingen kort
+        return 0; 
+    } 
+
+    tempCard = from->head;
+
+    for (int i = 0; i < index; ++i) {
+        if (tempCard == NULL) { //hvis man går for langt ift hvor mange kort der er 
+            break;
+        }
+        tempCard = tempCard->next;
+    }
+
+    if (tempCard == NULL) { // Kortet vi ledte efter fandtes
+        return 0; 
+    }
+
+    if (tempCard->prev == NULL) { // Vi tømmer bunken
+        from->head = NULL;
+        from->tail = NULL; 
+    } else { // Der er flere kort tilbage i bunken
+        from->tail = tempCard->prev;
+        from->tail->next = NULL; 
+        tempCard->prev = NULL; 
+        
+    }
+    // Skal nu indsættes i nye bunke
+    if (to->head == NULL) { // Hvis til bunken er tom
+        to->head = tempCard; //1. element peger på 1. element 
+
+    } else {
+        to->tail->next = tempCard; 
+        tempCard->prev = to->tail;
+
+    }
+
+    while (tempCard->next != NULL) {
+        tempCard = tempCard->next;
+    }
+
+    to->tail = tempCard;
+} 
+
+/***************************************** Logik funktioner *****************************************/
+
+void printConsole() {
+    printf("\n LAST Command: %s", inputCommandValue);
+    printf("\n Message: %s", message);
+    printf("\n INPUT >:");
+}
 
 void loadDefaultDeck() { 
     printf("\n LAST Command: %s", inputCommandValue);
@@ -134,88 +284,189 @@ void invalidInput() {
     printf("\n Message: Input not recognized. Type 'HELP' for a help message");
     printf( "\n INPUT >:");
 }
+
 void playerInput() {
 
-    char tempCommandValue;
+    char tempCommandValue; //[100]
 
-//MANGLER SPILLOGIK 
-// HVIS NU DET ER NOGLE AF DE TING HVOR DER KAN INDTASTES FILNAVN ETC: 
-//BESKEDERNE SKAL ERSTATTE TIDLIGERE BESKEDER OG IKKE VÆRE EN NY LINJE! 
+// Denne funktion validerer brugerinput ifm. spillogik, og derefter vælger den tilhørende spillogik
+
     do { 
-        tempCommandValue = scanf("%s[0-9a-zA-Z ]", inputCommandValue); //"%s"
-        
-        if (strcmp(inputCommandValue, "LD") == 0) {
-            loadDefaultDeck(); 
-            //Load default deck function  
-        } else if(strcmp(inputCommandValue, "SW") == 0) {
+        tempCommandValue = scanf("%s[0-9a-zA-Z ]", inputCommandValue); /*
+            fgets(tempCommandValue, 100, stdin);
+            if ( tempCommandValue[strlen(tempCommandValue)-1] == '\n') {
+                tempCommandValue[strlen(tempCommandValue)-1] = '\0';
+            } */
+
+        if (strcmp(inputCommandValue, "LD") == 0) { //Load default deck function  
+            loadDefaultDeck();
+            initializeDeck();
+            basicLD(); 
+            //dealCards();
+            
+        } else if(strcmp(inputCommandValue, "SW") == 0) { //Show deck function
             showDeck(); 
-            //Show deck function 
-        } else if(strcmp(inputCommandValue, "SI") == 0) {
+            printDeck();
+             
+        } else if(strcmp(inputCommandValue, "SI") == 0) { // Split shuffle function 
             splitDeck(); 
-            // Split shuffle function 
-        } else if (strcmp(inputCommandValue, "SR") == 0) {
+            
+        } else if (strcmp(inputCommandValue, "SR") == 0) { // Shuffle random function
             shuffleRandom();
-            // Shuffle random function
-        } else if (strcmp(inputCommandValue, "SD") == 0) {
+            
+        } else if (strcmp(inputCommandValue, "SD") == 0) { // Save current deck function 
             saveDeck(); 
-            // Save current deck function 
-        } else if (strcmp(inputCommandValue, "P") == 0) {
+            
+        } else if (strcmp(inputCommandValue, "P") == 0) { // Start game function
             startGame(); 
-            // Start game function 
-        } else if (strcmp(inputCommandValue, "Q") == 0) {
+             
+        } else if (strcmp(inputCommandValue, "Q") == 0) { // Quit current game function
             quitCurrentGame(); 
-            // Quit current game function
-        } else if (strcmp(inputCommandValue, "QQ") == 0) {
+            clearGame();
+            
+        } else if (strcmp(inputCommandValue, "QQ") == 0) { // Quit program
             quitGame(); 
-            // Quit program
-        } else if (strcmp(inputCommandValue, "HELP") == 0) {
+            
+        } else if (strcmp(inputCommandValue, "HELP") == 0) { // Help function 
             helpMessage(); 
-            // Help function 
-        } else { //If incorrect input is taken 
+            
+        } /* else if (tempCommandValue[0] == 'L' && tempCommandValue[1] == 'D'  // To load "LD <filename>". Requires the use of fgets and tempCommandValue as array.
+                && tempCommandValue[2] == ' ' && strlen(tempCommandValue)>5) { 
+                    // placeholder
+                    loadDefaultDeck();
+                    initializeDeck();
+                    basicLD(); 
+        } */ else { //If incorrect input is taken 
             invalidInput(); 
         }
-
+    
+        //Insert printing functions
+        //printDeck();
+        //printConsole();
     } while (strcmp(inputCommandValue, "QQ") != 0); //If QQ is taken then quit. This while statement is a formality for the loop. It will never run. 
 }
 
+void basicLD() {
+    char tempArray[20];
 
-void loadDeck() {
-    char tempArray[52];
     FILE *fp;
+    fp = fopen ("CardDeck.txt", "r");		// Open the file with 'read' option.	''
+    if(fp==NULL){ exit(-1);}
+    while(!feof(fp)){
 
-    fp = fopen("CardDeck.txt", "r"); // Open the file with 'read' option.
-
-    if (fp == NULL){ 
-        printf("Error! The file is not found or could not be opened \n");
-        exit(-1);
-    }     
-        for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 12; ++j) {
-            if (fgets(tempArray, 52, fp) != NULL){
-            }            
+       fgets(tempArray, 20, fp);
+       if (strlen(tempArray)>4) { 
+            //Placeholder remove all nodes
+       }
+       if(tempArray[strlen(tempArray)-1]== '\n'){
+            tempArray[strlen(tempArray)-1] = 0;
         }
-    fclose(fp);
+        addCard(end);
+        end->tail->suit = tempArray[1];
+        end->tail->value = tempArray[0];
     }
+    fclose(fp);
 }   
 
+// Initialize all piles
+void initializeDeck() {
+    for (int i=0 ; i < 12 ; i++) {
+        addPile();
+    }
+}
 
-int main() {
+// Sletter alle piles og kort i gamet
+void clearGame() { 
+    while (end != NULL) {
+        while (end->tail != NULL) {
+            removeCard(end);
+        } 
+        removePile();
+    }
+}
 
-    playerInput();
-    cardDeck();
-    loadDeck(); // from file
+// Printer deck på columns 
+void printDeck() {
+    struct Pile *tempPile = NULL, *tempFoundation = NULL; 
+    struct card *tempCard = NULL; 
+    int row = 0, column = 0, lastElementCount = 0, count = 0; 
+    tempFoundation = start->next->next->next->next->next->next->next; //for 8 (7+1) columns
+    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
+    while (1) { //infinite loop
+        tempPile = start; 
+        for (int i= 0 ; i < column ; i++) {
+            tempPile = tempPile->next;
 
-    insertCardsToColumns( 9,0);
-    insertCardsToColumns( 9,1);
-    insertCardsToColumns( 9,2);
-    insertCardsToColumns( 8,3);
-    insertCardsToColumns( 8,4);
-    insertCardsToColumns( 8,5);
-    insertCardsToColumns( 8,6);
+        } 
+        if (tempPile->head != NULL) {//navigere til hvilket kort vi er nået til
+            tempCard = tempPile->head;
+            count = 0; 
+            while (count < row) { // vil gerne hoppe fremad til næste node
+                tempCard = tempCard->next;
+                count++;
+                if (tempCard->next == NULL) {
+                    lastElementCount++;
+                }
+            }
+        
+        } else {
+            tempCard = NULL; 
+            lastElementCount++;
+        } 
+        //Printing below 
+        if (tempCard == NULL) {
+            //Nothing
+        } else {
+            if (tempCard->cardRevealed = 0) { // det må ikke vises hvad der er i skjulte kort "[]"
+                printf("[]");
 
-    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\t\t\t\n\n");
-    //printf("hell");
+            } else {
+                printf("%c%c", tempCard->value, tempCard->suit);         
+            }
+        }
+        printf("\t");  // hop til næste element
 
-    return 0;
+        column++;
+        if (column == 7) {
+            if (row == 0 || row == 2 || row == 4 || row == 6) {
+                if(tempFoundation->tail == NULL) {
+                    printf("[]");                
+                } else {
+                    printf("%c%c", tempFoundation->tail->value, tempFoundation->tail->suit);    
+                }
+                printf("\tF%d", row/2+1); 
+                if (tempFoundation->next != NULL){
+                    tempFoundation = tempFoundation->next;
+                }
 
-   }
+            }
+            row++;
+            printf("\n");
+            if (row > 5 && lastElementCount == 7) { // vi vil gerne breake / ud af infinite loop
+                break;
+            }
+
+        }
+        lastElementCount = 0; 
+
+        column = column % 7;
+    } // End infinite loop
+}
+
+void dealCards() {
+    struct Pile *tempPile = NULL; 
+    int count = 0; 
+    tempPile = start;
+
+    while (end->tail != NULL) { // 12th hidden list accessed
+        moveCard(end, tempPile);
+        count++;
+        if (count == 7) {
+            tempPile = start;
+        } else {
+            tempPile = tempPile->next;
+        }
+        count = count % 7; //makes it dynamic
+    }
+    
+}
